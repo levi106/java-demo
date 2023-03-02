ff64cbc69adb80cbc48a2247d7ca137b890b44ca
levi106 <16788901+levi106@users.noreply.github.com>
2023-03-03 03:06:00 +0900

    add PostConstruct

--- src/demo/src/main/java/com/example/demo/DemoApplication.java
+++ src/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.demo.services.LeakService;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 	static private int DefaultIntervalSec = 5;
@@ -23,6 +25,11 @@ public class DemoApplication {
 	@Value("${app.job.leak.enabled:false}")
     private boolean enableLeakJob;
 
+	@Value("${app.initial.memory.allocation:0}")
+	private int initialMemoryAllocation;
+
+	private byte[] array;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -31,6 +38,13 @@ public class DemoApplication {
 	public void scheduleRecurrently() {
 		if (enableLeakJob) {
 			jobScheduler.<LeakService>scheduleRecurrently(Duration.ofSeconds(DefaultIntervalSec), x -> x.executeMemoryLeakJob(DefaultLeakSize));
+		}
+	}
+	@PostConstruct
+	public void initialMemoryAllocation() {
+		if (initialMemoryAllocation > 0) {
+			log.info("Allocate {} bytes", initialMemoryAllocation);
+			array = new byte[initialMemoryAllocation];
 		}
 	}
 }
