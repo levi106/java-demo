e498cb58435ab3e69b65a32ff517d9225ea580f5
levi106 <16788901+levi106@users.noreply.github.com>
2023-03-02 14:05:47 +0900

    fix unit test

--- src/demo/src/test/java/com/example/demo/controllers/DbControllerTest.java
+++ src/demo/src/test/java/com/example/demo/controllers/DbControllerTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.DemoApplication;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(DbController.class)
+@SpringBootTest(classes = DemoApplication.class)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 public class DbControllerTest {
